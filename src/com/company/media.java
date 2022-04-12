47af1ed06481967de34c18fb9996e303954537aa
sergioleal2020 <cristoleal@gmail.com>
2022-04-12 11:20:22 -0300

    commit test

---
+++ src/com/company/media.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class media {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        float nota1, nota2,nota3, calc_media;
+        nota1 = 5;
+        nota2 = 7;  nota3 = 10;
+
+
+        calc_media = (nota1 + nota2 + nota3) / 2;
+
+        System.out.println("A media =" + calc_media);
+
+    }
+}
