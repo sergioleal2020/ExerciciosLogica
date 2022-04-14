fc7a43d78ff3da3034f7af2c3bb7edb171b0c891
sergioleal2020 <69284237+sergioleal2020@users.noreply.github.com>
2022-04-13 23:05:44 -0300

    Add files via upload
    Exercicios de logica programação pascal  ler media.

---
+++ media_ler.pas
@@ -0,0 +1,15 @@
+program media_ler;
+
+var
+nota1,nota2, cal_media: real;
+
+begin
+readln(nota1);
+readln(nota2);
+cal_media := (nota1 + nota2) / 2;
+writeln('A media =', cal_media);
+readln();
+end.
+
+
+
