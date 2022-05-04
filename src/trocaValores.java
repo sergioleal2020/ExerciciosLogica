8f590d63b822133a0d601b1b5ed0950dbcde7a7e
sergioleal2020 <cristoleal@gmail.com>
2022-05-03 23:17:58 -0300

    Exercicio logica programação correção operadores 03/05/2022 trocaValores em java

---
+++ src/trocaValores.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class trocaValores {
+
+    public static void main (String args[])
+    {
+
+        int A, B, Auxiliar;
+        A= 5;
+        B =10;
+
+        JOptionPane.showMessageDialog(null,"A = "+A+"  e B= "+B);
+                Auxiliar = A;
+        A  =B;
+        B =Auxiliar;
+
+
+
+    }
+
+
+}
